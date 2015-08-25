376569df2f435ddb5d087c3fb26cba02e24ea4d4
Soheil <ysoheil@gmail.com>
2015-08-25 14:39:30 -0700

    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Bug::Application.config.secret_token = '5246434f684e795340d07554e2c5547e397ef23122229c8e594ec5c830692e4150bda87cdce9b83f2c389b98abb888a7fb8a224cfff7522f366529de9152be21'
